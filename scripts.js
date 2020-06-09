19907c7b00a388b0f26ee37e769b0e5ff386efb5
Tran Kim Nikoletta <trankimniki@gmail.com>
2020-06-09 19:56:45 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -14,42 +14,58 @@ let currentPhoto = 0;
 let imagesData = [
     {
         photo: "images/flowers.jpg",
-        title: "Sziklakert",
+        title: "Sziklakert, közelről",
         text: "A hátsó kertben található meg, ahol a látványa azonnal odavonzza a kíváncsi tekinteteket."
     },
     {
         photo: "images/flowers_1.jpg",
-        title: "Sziklakert",
+        title: "Sziklakert, távolról",
         text: "A hátsó kertben található meg, ahol a látványa azonnal odavonzza a kíváncsi tekinteteket."
     },
     {
         photo: "images/flowers_2.jpg",
-        title: "Sziklakert",
-        text: "A hátsó kertben található meg, ahol a látványa azonnal odavonzza a kíváncsi tekinteteket."
+        title: "Színes virágok",
+        text: "Ezek a vidám virágok mindenkit vidámmá tesznek."
     },
     {
         photo: "images/green.jpg",
-        title: "Sziklakert",
-        text: "A hátsó kertben található meg, ahol a látványa azonnal odavonzza a kíváncsi tekinteteket."
+        title: "Bazsalikom",
+        text: "Niki gondozza, és jelenleg többször van bent, mint kint."
     },
     {
         photo: "images/lights.jpg",
-        title: "Sziklakert",
-        text: "A hátsó kertben található meg, ahol a látványa azonnal odavonzza a kíváncsi tekinteteket."
+        title: "Éjjeli fények",
+        text: "A zöld növényes részeket nagyon feldobja egy díszes kerti lámpa."
     },
     {
         photo: "images/little_lake.jpg",
-        title: "Sziklakert",
-        text: "A hátsó kertben található meg, ahol a látványa azonnal odavonzza a kíváncsi tekinteteket."
+        title: "Tavacska",
+        text: "Kisebb-nagyobb halak otthona, amit szép kavicsok és élénk zöld fű vesz körül."
     }
 
 ];
 
-$('#photo').attr('src', imagesData[currentPhoto].photo)
-$('#photo-title').append(imagesData[currentPhoto].title)
-$('#photo-text').append(imagesData[currentPhoto].text)
+$('#photo').attr('src', imagesData[currentPhoto].photo);
+$('#photo-title').text(imagesData[currentPhoto].title);
+$('#photo-text').text(imagesData[currentPhoto].text);
 
-$('#next').click((event) => {
-    ;
-})
+$('#next').click(() => {
+    if (currentPhoto < imagesData.length - 1) {
+    currentPhoto++;
+  }
+    $('#photo').attr('src', imagesData[currentPhoto].photo);
+    $('#photo-title').text(imagesData[currentPhoto].title);
+    $('#photo-text').text(imagesData[currentPhoto].text);
+ }); 
+
+
+
+$('#back').click(() => {
+    if (currentPhoto > 0) {
+    currentPhoto--;
+  }  
+    $('#photo').attr('src', imagesData[currentPhoto].photo);
+    $('#photo-title').text(imagesData[currentPhoto].title);
+    $('#photo-text').text(imagesData[currentPhoto].text);
+});
 
